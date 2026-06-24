e7e6d435f3bd3ddbbccdb4537a3aaa3cbf94b1a1
biyalizabraham08 <biyalizabraham08@gmail.com>
2026-06-24 17:07:20 +0530

    Add Render build script and static serving

--- src/app.js
+++ src/app.js
@@ -10,16 +10,25 @@ const followUpRouters = require ('./routes/followupRoutes');
 const  dashboardRoutes= require('./routes/dashboardRoutes')
 
 
-// Routes
-app.get('/', (req, res) => {
-  res.send('Welcome to CRM Follow-up System API');
-});
+const path = require('path');
 
+// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/lead', leadRoutes);
 app.use('/api/followup', followUpRouters);
-app.use('/api/dashboard',dashboardRoutes);
+app.use('/api/dashboard', dashboardRoutes);
 
+// Serve frontend in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('Welcome to CRM Follow-up System API');
+  });
+}
 
 module.exports = app;
